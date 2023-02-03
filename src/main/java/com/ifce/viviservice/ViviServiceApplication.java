9dc2b9f76ca26b80108bc215c54712bcaa31a135
unknown <camila.rmorais73@gmail.com>
2023-02-03 01:14:36 -0300

    Configuração Inicial

---
+++ src/main/java/com/ifce/viviservice/ViviServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ifce.viviservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViviServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViviServiceApplication.class, args);
+	}
+
+}
